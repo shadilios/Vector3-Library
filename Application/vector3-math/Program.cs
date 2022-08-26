8574efd08481b81ccf11de01187deb034b7d9e70
shadilios <70282602+shadilios@users.noreply.github.com>
2022-08-26 21:36:10 +0300

    initial

---
+++ Application/vector3-math/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace vector3_math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3 x = new Vector3(-1, 2, 5);
+            Vector3 y = new Vector3(2, 7, 17);
+
+
+            Console.WriteLine($"Vector X = {x}");
+            Console.WriteLine($"Vector X magnitude = {x.Magnitude}");
+            Console.WriteLine($"Vector X normalized = {x.Normalized}");
+
+
+            Console.WriteLine($"Vector Y = {y}");
+            Console.WriteLine($"Vector Y magnitude = {y.Magnitude}");
+            Console.WriteLine($"Vector Y normalized = {y.Normalized}");
+
+
+
+            Console.WriteLine($"Dot product of X & Y: " + Vector3.DotProduct(x, y));
+
+            Console.WriteLine($"Cross product of X & Y: " + Vector3.CrossProduct(x, y));
+        }
+    }
+}
---
+++ Application/vector3-math/Vector3.cs
@@ -0,0 +1,125 @@
+﻿namespace vector3_math
+{
+    public class Vector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        /// <summary>
+        /// Returns the current vector normalized
+        /// </summary>
+        public Vector3 Normalized
+        {
+            get { return new Vector3(X / (float)Magnitude, Y / (float)Magnitude, Z / (float)Magnitude); }
+        }
+
+        /// <summary>
+        /// Returns the magnitude of the current vector
+        /// </summary>
+        public double Magnitude
+        {
+            get { return MathF.Sqrt((X * X) + (Y * Y) + (Z * Z)); }
+            
+        }
+
+        /// <summary>
+        /// Returns the magnitude of the current vector squared
+        /// </summary>
+        public double MagnitudeSquared
+        {
+            get { return Magnitude * Magnitude; }
+        }
+
+        /// <summary>
+        /// Creates a zero vector (0, 0, 0)
+        /// </summary>
+        public Vector3()
+        {
+            X = Y = Z = 0;
+        }
+
+        /// <summary>
+        /// Creates a vector in 3D space with (x, y, z) dimensions
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="z"></param>
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}, {Z})";
+        }
+
+        /// <summary>
+        /// Normalizes the Vector3 so it becomes a unit Vector
+        /// </summary>
+        public void Normalize()
+        {
+            X = X / (float)Magnitude;
+            Y = Y / (float)Magnitude;
+            Z = Z / (float)Magnitude;
+        }
+
+        /// <summary>
+        /// Performs CrossProduct between firstVector & secondVector
+        /// </summary>
+        /// <param name="firstVector"></param>
+        /// <param name="secondVector"></param>
+        /// <returns></returns>
+        public static double DotProduct(Vector3 firstVector, Vector3 secondVector)
+        {
+            return (
+                (firstVector.X * secondVector.X)
+                + (firstVector.Y * secondVector.Y)
+                + (firstVector.Z * firstVector.Z)
+                );
+
+            // or magnitude1 * magnitude2 * Cos theta
+
+        }
+
+        /// <summary>
+        /// Performs CrossProduct between firstVector & secondVector
+        /// </summary>
+        /// <param name="firstVector"></param>
+        /// <param name="secondVector"></param>
+        /// <returns></returns>
+        public static Vector3 CrossProduct(Vector3 firstVector, Vector3 secondVector)
+        {
+
+            float newX = firstVector.Y*secondVector.Z - firstVector.Z*secondVector.Y;
+            float newY = firstVector.Z*secondVector.X - firstVector.X*secondVector.Z;
+            float newZ = firstVector.X*secondVector.Y - firstVector.Y*secondVector.X; 
+
+            return new Vector3(newX, newY, newZ);
+
+            /*
+            or a × b = |a| * |b| * sin(θ) * n
+            where n: unit vector at right angles to both firstVector & secondVector
+            */
+        }
+
+        /// <summary>
+        /// Scales the vector by multiplying it by "number" argument
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public void Scale(float number)
+        {
+            X *= number;
+            Y *= number;
+            Z *= number;
+        }
+    }
+
+
+}
